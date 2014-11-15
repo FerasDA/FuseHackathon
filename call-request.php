4b08409678b1ba654a4ebb1cf3fd38f52d78c9bf
Congshu Wang <congshuwang@gmail.com>
2014-11-15 06:29:32 -0500

    added contact page with some twilio files

---
+++ call-request.php
@@ -0,0 +1,30 @@
+
+<?php
+    // Include the Twilio PHP library
+    require 'twilio-php-master/Services/Twilio.php';
+ 
+    // Twilio REST API version
+    $version = "2010-04-01";
+ 
+    // Set our Account SID and AuthToken
+   $sid = 'ACea19bb9852f3b05ae3b682d66637cfd9';
+	$token = 'd4dd54e452ffa18dea1e1c8ba670a9dc';
+     
+    // A phone number you have previously validated with Twilio
+    $phonenumber = '+17652764084';
+     
+    // Instantiate a new Twilio Rest Client
+    $client = new Services_Twilio($sid, $token, $version);
+ 
+    try {
+        // Initiate a new outbound call
+        $call = $client->account->calls->create(
+            $phonenumber, // The number of the phone initiating the call
+            '+17657200874', // The number of the phone receiving call
+            'http://demo.twilio.com/welcome/voice/' // The URL Twilio will request when the call is answered
+        );
+        echo 'Started call: ' . $call->sid;
+    } catch (Exception $e) {
+        echo 'Error: ' . $e->getMessage();
+    }
+
